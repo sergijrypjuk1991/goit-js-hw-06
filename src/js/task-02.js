01c5f254f96e7f6a2c33805cc117d0bd3213adbd
sergijrypjuk1991 <101973508+sergijrypjuk1991@users.noreply.github.com>
2022-09-22 13:09:18 +0300

    111

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,45 +7,15 @@ const ingredients = [
   'Condiments',
 ];
 
-const firstEl = document.createElement('li');
-  firstEl.classList.add('item')
-  firstEl.textContent = ingredients[0];
-
-  const secEl = document.createElement('li');
-  secEl.classList.add('item')
-  secEl.textContent = ingredients[1];
-  
-const trEl = document.createElement('li');
-  trEl.classList.add('item')
-  trEl.textContent = ingredients[2];
-  
-const fourEl = document.createElement('li');
-  fourEl.classList.add('item')
-  fourEl.textContent = ingredients[3];
-  
-const fivEl = document.createElement('li');
-  fivEl.classList.add('item')
-  fivEl.textContent = ingredients[4];
-
-  const sixEl = document.createElement('li');
-  sixEl.classList.add('item')
-  sixEl.textContent = ingredients[5];
-  
-const listEl = document.querySelector('#ingredients');
-
-listEl.append(firstEl, secEl, trEl, fourEl, fivEl, sixEl);
 
 
+const allEl = ingredients.map (ingredient => {
 
-//ingredients.forEach(ingredient => {
-
-  //const addStringEl = document.createElement('li')
-  //addStringEl.classList.add('item')
-  //addStringEl.textContent = ingredient;
-  
-  //list.append(addStringEl);
+  const addStringEl = document.createElement('li');
+  addStringEl.classList.add('item');
+  addStringEl.textContent = ingredient;
+  return addStringEl;
   //console.log(addStringEl);
-//})
-
-//const list = document.querySelector('#ingredients');
-//list.append()
+});
+const listEl = document.querySelector('#ingredients');
+listEl.append(...allEl);
